26063852039857dc781d7513e790ca9bdded76db
sangyeon-park <tkddus0421@gmail.com>
2026-07-28 18:12:12 +0900

    Add entity.py

---
+++ entity.py
@@ -0,0 +1,25 @@
+import config
+import random
+
+class Agent:
+    x: int
+    y: int
+    r: int
+    speed: int
+
+
+class Ball:
+    x: int
+    y: int
+    vx: int
+    vy: int
+    r: int
+
+    def __init__(self):
+        # 첫 충돌 판정을 무시하기 위해 반지름만큼 떨어진 거리에 공 생성
+        self.x = config.BALL_RADIUS
+        self.y = config.BALL_RADIUS
+
+        self.vx = random.randint(config.MIN_BALL_SPEED, config.MAX_BALL_SPEED)
+        self.vy = random.randint(config.MIN_BALL_SPEED, config.MAX_BALL_SPEED)
+        self.r = config.BALL_RADIUS
