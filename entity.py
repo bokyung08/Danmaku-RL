60c4629da79d707453f2f586d6f73ff7d7e768ca
bokyung08 <bokyung082808@gmail.com>
2026-07-29 10:50:44 +0900

    rollback entity.py

--- entity.py
+++ entity.py
@@ -21,7 +21,7 @@ class Ball:
     vy: int
     r: int
 
-    def __init__(self):
+    def __init__(self, x, y, vx, vy):
         # 첫 충돌 판정을 무시하기 위해 반지름만큼 떨어진 거리에 공 생성
         self.x = config.BALL_RADIUS
         self.y = config.BALL_RADIUS
