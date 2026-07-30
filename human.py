e4b81643a57310db858b3176d3fea56b6e71a451
bokyung08 <bokyung082808@gmail.com>
2026-07-30 11:05:50 +0900

    Modify human.py

--- human.py
+++ human.py
@@ -28,13 +28,14 @@ def move(up, down, left, right):
     return config.ACTION_STOP
 
 def play_loop(game, renderer, clock):
-    fixed_dt = 1.0 / config.PHYSICS_FPS # 초당 60번 게임 step을 실행한다고 가정하면 한 번 실행하는 시간은 1/60 , 한 스텝이 진행되는 시간을 고정한다. 
-    accumulator = 0.0 # 시간 누적기 
+    fixed_dt = 1.0 / config.PHYSICS_FPS # 게임이 실행되는 주기를 고정 
+                                        # 초당 60번 게임 step을 실행한다고 가정하면 한 번 실행하는 시간은 1/60 , 한 스텝이 진행되는 시간을 고정한다. 
+    accumulator = 0.0 # 시간 누적
 
     while True:
         frame_dt = clock.tick(config.RENDER_FPS)/1000.0 # 실제로 흐른 시간 측정(ms) / 1000 
-        frame_dt = min(frame_dt, config.MAX_FRAME_TIME) # 정지 등으로 인해 실제로 흐른 시간 값이 커져도 물리 업데이트가 한꺼번에
-                                                        # 지나치게 많이 실행되는 것을 방지
+        frame_dt = min(frame_dt, config.MAX_FRAME_TIME) # 정지 등으로 인해 실제로 흐른 시간 값이 커져도 물리 업데이트가 
+                                                        # 한꺼번에 많이 실행되는 것을 방지
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -47,10 +48,10 @@ def play_loop(game, renderer, clock):
                 
         action = move(*get_key())
         if game.state.alive: 
-            accumulator += frame_dt * config.GAME_SPEED # 실제 흐른 시간 * 게임 속도 
-            while accumulator >= fixed_dt: # STEP 한 번을 실행할 만큼 시간이 누적됐는지 확인 
+            accumulator += frame_dt * config.GAME_SPEED # 누적할 게임 시간 =  실제 흐른 시간 * 게임 속도 
+            while accumulator >= fixed_dt and game.state.alive:  # 누적된 시간 >= 게임 실행 주기 시간 
                 game.step(action)
-                accumulator -= fixed_dt # 한 번 실행했으므로 실행한 시간 차감 
+                accumulator -= fixed_dt     # 한 번 실행했으므로 실행한 시간 차감 
         else: 
             accumulator = 0.0
         renderer.draw(game)
